bd15d9fda5006a68c20c96868abcc216419d5c8c
Douglas Chen <dougpuob@gmail.com>
2020-07-29 07:38:25 +0800

    Add build script files for Linux environment.

---
+++ build-linux-debug.ps1
@@ -0,0 +1,21 @@
+if (-Not (Test-Path 'build-debug')) {
+	New-Item -Path 'build-debug' -ItemType Directory
+}
+
+Push-Location build-debug
+
+cmake -DLLVM_ENABLE_ABI_BREAKING_CHECKS=0       `
+      -DLLVM_BUILD_TOOLS=ON                     `
+      -DLLVM_BUILD_EXAMPLES=ON                  `
+      -DLLVM_INCLUDE_TESTS=ON                   `
+      -DLLVM_TARGETS_TO_BUILD="X86"             `
+      -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra"  `
+      -DCMAKE_BUILD_TYPE=Debug                  `
+      -G "Ninja"                                `
+      ..\llvm                                   `
+      | tee ..\build-cmake-linux-clang-config.log
+
+
+cmake --build . --config Debug  | tee ..\build-cmake-linux-clang-make.log
+
+Pop-Location
